6c9d0be316035f48bb06fb5e4b89c58584a6713f
IWannaWish <138825123+IWannaWish@users.noreply.github.com>
2025-07-24 23:59:39 +0300

    добавил ethusd server

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -5,27 +5,33 @@ import (
 	"log"
 	"net"
 
-	apigrpc "github.com/IWannaWish/ethusd-converter/internal/api/grpc"
-	"github.com/IWannaWish/ethusd-converter/internal/applog"
-	"github.com/IWannaWish/ethusd-converter/internal/config"
-	"github.com/IWannaWish/ethusd-converter/internal/eth/abi"
-	"github.com/IWannaWish/ethusd-converter/internal/eth/source"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+
+	applog "github.com/IWannaWish/ethusd-converter/internal/applog"
+	"github.com/IWannaWish/ethusd-converter/internal/config"
+	"github.com/IWannaWish/ethusd-converter/internal/eth/abi"
+	"github.com/IWannaWish/ethusd-converter/internal/eth/source"
+
+	apigrpc "github.com/IWannaWish/ethusd-converter/internal/api/grpc"
+	ethusd_pb "github.com/IWannaWish/ethusd-converter/proto/ethusd/gen"
+
 	"google.golang.org/grpc"
 )
 
 func main() {
 	_ = godotenv.Load("config.env")
-	cfg := config.Load()
 
+	cfg := config.Load()
 	logger := applog.NewLogger(cfg)
 	ctx := applog.WithRequestID(context.Background(), uuid.NewString())
 
-	logger.Info(ctx, "Starting gRPC server...", applog.String("rpc_url", cfg.RPCURL))
+	logger.Info(ctx, "gRPC-сервер запускается...",
+		applog.String("module", "grpc"),
+		applog.String("rpc_url", cfg.RPCURL),
+	)
 
-	// Подключаемся к Ethereum
 	client, err := ethclient.Dial(cfg.RPCURL)
 	if err != nil {
 		logger.Error(ctx, "Ошибка подключения к Ethereum", applog.WithStack(err)...)
@@ -47,27 +53,24 @@ func main() {
 
 	sources, err := source.BuildAssetSources(ctx, logger, cfg.Tokens, client, erc20ABI, feedABI)
 	if err != nil {
-		logger.Error(ctx, "Ошибка построения источников", applog.WithStack(err)...)
+		logger.Error(ctx, "Ошибка построения источников активов", applog.WithStack(err)...)
 		return
 	}
 
 	assetService := source.NewAssetService(sources, logger)
+
 	grpcServer := grpc.NewServer()
-
 	server := apigrpc.NewEthusdGRPCServer(assetService)
+	ethusd_pb.RegisterEthusdConverterServer(grpcServer, server)
 
-	// Регистрация gRPC сервера
-	apigrpc.RegisterEthusdConverterServer(grpcServer, server)
-
-	lis, err := net.Listen("tcp", ":50051")
+	listener, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		logger.Error(ctx, "Ошибка запуска TCP-листенера", applog.WithStack(err)...)
 		return
 	}
 
-	logger.Info(ctx, "gRPC сервер слушает на порту :50051")
-
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("gRPC server error: %v", err)
+	logger.Info(ctx, "gRPC сервер слушает на :50051")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Ошибка запуска gRPC сервера: %v", err)
 	}
 }
